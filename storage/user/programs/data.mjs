68b47b33000254930ca1cfc95792d4338a7e6792
UnitedCatdom <kgoszczycki3636@gmail.com>
2022-08-28 20:20:52 +0200

    Added programs: YouTube, Spotify

--- storage/user/programs/data.mjs
+++ storage/user/programs/data.mjs
@@ -1,26 +1,42 @@
 export let programs = [
     {
-        name: "Google",
-        id: "google",
-        icon: "storage/system/imgs/icons/programs/google.png",
-        src: "https://google.com",
-        on_taskbar: true,
-        on_desktop: true
-    },
-    {
-        name: "Gmail",
-        id: "gmail",
-        icon: "storage/system/imgs/icons/programs/gmail.png",
-        src: "https://gmail.com/",
-        on_taskbar: true,
-        on_desktop: true
-    },
-    {
         name: "System settings",
         id: "systemsettings",
         icon: "storage/system/imgs/icons/programs/systemsettings.png",
         src: "storage/user/programs/system_settings/index.html",
         on_taskbar: true,
+        on_desktop: false
+    },
+    {
+        name: "Google",
+        id: "google",
+        icon: "storage/user/programs/_app_icons/google.png",
+        src: "https://google.com",
+        on_taskbar: false,
+        on_desktop: false
+    },
+    {
+        name: "Gmail",
+        id: "gmail",
+        icon: "storage/user/programs/_app_icons/gmail.png",
+        src: "https://gmail.com/",
+        on_taskbar: false,
+        on_desktop: false
+    },
+    {
+        name: "YouTube",
+        id: "youtube",
+        icon: "storage/user/programs/_app_icons/youtube.png",
+        src: "https://youtube.com",
+        on_taskbar: false,
+        on_desktop: false
+    },
+    {
+        name: "Spotify",
+        id: "spotify",
+        icon: "storage/user/programs/_app_icons/spotify.png",
+        src: "https://open.spotify.com",
+        on_taskbar: false,
         on_desktop: false
     }
 ]
